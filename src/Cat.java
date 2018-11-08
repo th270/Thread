f290e53f3376c52bd46b6d4325994971ef16b5b6
th270 <448725485@qq.com>
2018-11-08 19:06:23 +0800

    add a new class Cat

---
+++ src/Cat.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Administrator on 2018/11/8.
+ */
+public class Cat {
+    private String name;
+    private Integer age;
+    private String color;
+
+    public Cat() {
+    }
+
+    public Cat(String name, Integer age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" + "name='" + name + '\'' + ", age=" + age + ", color='" + color + '\'' + '}';
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int sum = add(1, 2);
+        System.out.println("sum == " + sum);
+    }
+    
+    public static int add(int a , int b ){
+        return a + b;
     }
 }
